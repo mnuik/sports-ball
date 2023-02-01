cc9fd7ab2845ecd1c7158ca18e6553015f2adcdb
Mira K Muikku <muikkumk@gmail.com>
2023-02-01 10:33:23 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div>
+        <h4>SportsBall Lounge</h4>
+      </div>
+      </header>
+      <body>
+        <div class="topnav">
+          <a href="#sports">Sports</a>
+          <a href="#tables">Tables</a>
+          <a href="#finland">Finland NTs</a>
+          <a href="#eesti">Eesti NTs</a>
+          <a href="#arsenal">Arsenal</a>
+          <a href="#manu">ManU</a>
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to SportsBall Lounge!
         </p>
         <a
           className="App-link"
@@ -15,9 +29,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reäct
         </a>
-      </header>
+      </body>
     </div>
   );
 }
