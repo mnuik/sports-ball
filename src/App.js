6664581cf31103b7b011c2ccac4de299b8f70d5e
Mira K Muikku <muikkumk@gmail.com>
2023-02-15 10:47:45 +0200

    Cleanups

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navigation from "./components/Navigation";
 import Header from "./components/Header";
 import Tables from "./components/Tables";
@@ -7,49 +7,20 @@ import Estonia from "./components/Estonia";
 import Arsenal from "./components/Arsenal";
 import ManU from "./components/ManU";
 // import Button from "./components/Button";
-import bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//       <img src="BORBY 1.png" className="logo-borby" alt="logo" />
-//         <h4>SportsBall Lounge</h4>
-//       </header>
-//       <body>
-//         <div class="topnav">
-//           <a href="#sports">Sports</a>
-//           <a href="#tables">Tables</a>
-//           <a href="#finland">Finland NTs</a>
-//           <a href="#estonia">Estonia NTs</a>
-//           <a href="#arsenal">Arsenal</a>
-//           <a href="#manu">ManU</a>
-//         </div>
-//         <p>
-//           Welcome to SportsBall Lounge!
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn Reäct
-//         </a>
-//       </body>
-//     </div>
-//   );
-// }
 function App() {
+  useEffect(() => {
+    window.scrollTo(0,0);
+    document.documentElement.scrollTop=0;
+  })
 return (
   <>
   <Header />
   <Navigation />
-  
-  {/* <Tables /> 
-  <Sports />
+  <Tables /> 
+  {/* <Sports />
   <Finland />
   <Estonia />
   <Arsenal />
--- src/components/Tables.js
+++ src/components/Tables.js
@@ -1,41 +1,73 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import './components.css';
-import { Dropdown } from 'bootstrap';
+import { Dropdown } from 'react-bootstrap';
+import LeagueData from './LeagueData';
 
 
 function Tables() {
     const [league, setLeague] = React.useState();
     const [id, setId] = React.useState('eng.1');
-    const [season, setseason] = React.useState('2022');
+    const [season, setSeason] = React.useState('2022-2023');
     const [tournament, setTournament] = React.useState('Premier League');
 
+    const options = {
+        method: 'GET',
+        url: 'https://api-football-v1.p.rapidapi.com/v3/leagues',
+        headers: {
+          'X-RapidAPI-Key': 'e98a94efbamshc8e8072e90d09d0p14aefbjsne248c682f327',
+          'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+        }
+      };
     useEffect(() => {
-        axios.get('https://api-football-standings.azharimm.site/leagues').then(res => {
-            setLeague(res.data.data)
-        }).catch(err => {
-            console.log(err)
-        })
-    }, [])
+        axios.request(options).then(function (response) {
+            console.log(response.data);
+        }).catch(function (error) {
+            console.error(error);
+        });
+  
+    })
+
+    const year = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022'];
 
     return (
-        <div className='grid' id='standings'>
-            <Dropdown className='drop'>
-                <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                    Select League
-                </Dropdown.Toggle>
+        <div className='grid' id='tables'>
+            <div className='table-styles'>
+                <Dropdown className='drop'>
+                    <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                        Select League
+                    </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                    {league && league.map((league, key) => {
-                        return <Dropdown.Item key={key} onClick={() => {
-                            setId(league.id)
-                            setTournament(league.name)
-                        }}>{league.name}</Dropdown.Item>
-                    })}
-                </Dropdown.Menu>
-            </Dropdown>
+                    <Dropdown.Menu>
+                        {league && league.map((league, key) => {
+                            return <Dropdown.Item key={key} onClick={() => {
+                                setId(league.id)
+                                setTournament(league.name)
+                                setLeague(league.name)
+                            }}>{league.name}</Dropdown.Item>
+                        })}
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                <Dropdown className='drop'>
+                    <Dropdown.Toggle variant='primary' id='dropdown-basic'>
+                        Select Season
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        {year && year.map((year, key) => {
+                            return <Dropdown.Item key={key} onClick={() => {
+                                setSeason(year)
+                            }}>{year}</Dropdown.Item>
+                        })}
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
+            <div className='tables'>
+                <LeagueData season={season} id={id} tournament={tournament} />
+            </div>
         </div>
-    );
+    )
 }
 
 
